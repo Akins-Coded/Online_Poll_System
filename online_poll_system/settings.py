1f6c09bf1a51fa2ea30b609ff1b5ea766125af80
Akins-coded <akindipemuheez@gmail.com>
2025-09-25 09:25:20 +0100

    CSRF

--- online_poll_system/settings.py
+++ online_poll_system/settings.py
@@ -159,7 +159,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 # --------------------------
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
