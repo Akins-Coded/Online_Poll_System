add2b9a21fe3076159e7f789a08b6ba1dcd9b8f9
Akins-Coded <akindipemuheez@gmail.com>
2025-12-08 17:10:46 +0100

    alloweedd_Host

--- online_poll_system/settings.py
+++ online_poll_system/settings.py
@@ -209,7 +209,7 @@ EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",   # React frontend local
     "http://127.0.0.1:8000",   # Django local
-    "https://codedman.pythonanywhere.com",  # Production domain
+    "https://vote-online.onrender.com",  # Production domain
     "https://vote-poll.netlify.app",  # frontend domain
 ]
 
